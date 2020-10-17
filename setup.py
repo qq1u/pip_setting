47362b41d7ce84ac366f6461848755ad85d52147
Hugh <609799548@qq.com>
2020-10-17 13:15:00 +0800

    0.0.1 version

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from distutils.core import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pip-setting_test',
+    version='0.0.1',
+    author='丘家劲',
+    author_email='609799548@qq.com',
+    description='快速设置pip镜像源的工具',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/13528080556/pip_setting',
+    entry_points={
+        'console_scripts': [
+            'pip_setting=pip_setting:run'
+        ]
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
