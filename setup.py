089c8836fa3b30e50e03282085e85ca93e14ed5d
丘家劲 <qiujiajin@dt-edu.cn>
2020-12-15 16:37:36 +0800

    add MIT

--- setup.py
+++ setup.py
@@ -6,16 +6,20 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='pip-setting',
-    version='0.0.3',
+    version='0.0.4',
     author='丘家劲',
     author_email='609799548@qq.com',
     description='快速设置pip镜像源的工具',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    license='MIT Licence',
     url='https://github.com/13528080556/pip_setting',
     entry_points={
         'console_scripts': [
-            'pip-setting=pip_setting:run'
+            'pip-setting=pip_setting:run',
+            'pip_setting=pip_setting:run',
+            'pip3-setting=pip_setting:run',
+            'pip3_setting=pip_setting:run'
         ]
     },
     packages=setuptools.find_packages(),
