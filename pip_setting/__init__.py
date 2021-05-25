21a44c0a8861b37b9b8ce9f6a5e9a3e1f1c2ce5d
Hugh <609799548@qq.com>
2021-05-25 23:33:40 +0800

    source add tencent

--- pip_setting/__init__.py
+++ pip_setting/__init__.py
@@ -13,7 +13,11 @@ qh = ('[global]', 'index-url = https://pypi.tuna.tsinghua.edu.cn/simple',
       '[install]', 'trusted-host=pypi.tuna.tsinghua.edu.cn')
 db = ('[global]', 'index-url = https://pypi.doubanio.com/simple',
       '[install]', 'trusted-host=pypi.doubanio.com')
-mirrors = {'aliyun': ali, 'qinghua': qh, 'douban': db, '1': ali, '2': qh, '3': db}
+tx = ('[global]', 'index-url = https://mirrors.cloud.tencent.com/pypi/simple',
+      '[install]', 'trusted-host=mirrors.cloud.tencent.com')
+
+mirrors = {'aliyun': ali, 'qinghua': qh, 'douban': db, 'tencent': tx, '1': ali, '2': qh, '3': db, '4': tx}
+
 user_home = os.path.expanduser('~')
 pip_dir_name, pip_file_name = ('pip', 'pip.ini') if WIN else ('.pip', 'pip.conf')
 pip_dir_path = os.path.join(user_home, pip_dir_name)
@@ -45,15 +49,14 @@ def run():
         remote_pip()
     else:
         print('使用此工具可切换pip镜像源')
-        print('0、官方源\n1、阿里源\n2、清华源\n3、豆瓣源\n4、退出')
+        print('0、官方源\n1、阿里源\n2、清华源\n3、豆瓣源\n4、腾讯源\n5、退出')
         while True:
             opt = input('请输入序号: ')
-            if opt in ('0', '1', '2', '3', '4'):
-                if opt in '123':
+            if opt in ('0', '1', '2', '3', '4', '5'):
+                if opt in '1234':
                     create_pip_file(opt)
                 elif opt == '0':
                     remote_pip()
                 break
             else:
                 print('不支持操作选项! 请输入0-4')
-
